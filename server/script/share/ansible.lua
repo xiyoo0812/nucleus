be4c6c9840c1e3337d57f245e91d67b99b92bec0
xiyoo0812 <xiyoo0812@163.com>
2020-10-21 21:28:36 +0800

    优化代码实现

--- server/script/share/ansible.lua
+++ server/script/share/ansible.lua
@@ -10,23 +10,30 @@ local jdecode   = json.decode
 
 ansible = {}
 
+local function parse_task_res(line)
+    local out_token = smatch(line, '=> {.+}')
+    if out_token then
+        local out_res = jdecode(ssub(out_token, 3))
+        if not out_res.skipped then
+            return {
+                cmd = out_res.cmd,
+                delta = out_res.delta,
+                stdout = out_res.stdout,
+                stderr = out_res.stderr,
+            }
+        end
+    end
+end
+
 local function parse_ansible_res(output)
     local cur_task
     local ansible_res = {}
     local lines = ssplit(output, "\n")
     for _, line in ipairs(lines) do
         if cur_task then
-            local out_token = smatch(line, '=> {.+}')
-            if out_token then
-                local out_res = jdecode(ssub(out_token, 3))
-                if not out_res.skipped then
-                    ansible_res[cur_task] = {
-                        cmd = out_res.cmd,
-                        delta = out_res.delta,
-                        stdout = out_res.stdout,
-                        stderr = out_res.stderr,
-                    }
-                end
+            local task_res = parse_task_res(line)
+            if task_res then
+                ansible_res[cur_task] = task_res
                 cur_task = nil
             end
         else
