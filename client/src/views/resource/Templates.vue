ec50ba2111b0893d12c07284104ca75ae2e5ad27
xiyoo0812 <xiyoo0812@163.com>
2020-11-19 20:55:37 +0800

    修改界面

--- client/src/views/resource/Templates.vue
+++ client/src/views/resource/Templates.vue
@@ -4,7 +4,6 @@
         <el-alert :closable="false" type="success" title="负责管理配置模板。"/>
         <el-button-group style="margin-top:10px; margin-bottom:10px;">
             <el-button type="primary" style="margin-right:10px;" @click="handleCreate">添加</el-button>
-            <Selecter v-model="environ" :option="environ" clear="true" placeholder="请选择环境" :options="$store.getters.environs"/>
         </el-button-group>
         <el-table stripe v-loading="listLoading" style="width: 100%" :data="filterTemplates">
             <el-table-column label="名称">
@@ -12,9 +11,6 @@
             </el-table-column>
             <el-table-column label="描述">
                 <template slot-scope="scope"><span >{{ scope.row.desc }}</span></template>
-            </el-table-column>
-            <el-table-column label="环境">
-                <template slot-scope="scope"><span >{{ formatEnviron(scope.row) }}</span></template>
             </el-table-column>
             <el-table-column label="创建者">
                 <template slot-scope="scope"><span >{{ scope.row.creator }}</span></template>
@@ -40,19 +36,8 @@
             <el-form-item label="描述" prop="desc">
                 <el-input v-model="form.desc" placeholder="输入描述"/>
             </el-form-item>
-            <el-form-item label="环境" prop="environ">
-                <Selecter v-model="form.environ" :option="form.environ" :options="$store.getters.environs"/>
-            </el-form-item>
             <el-form-item label="模板参数" prop="args">
-                <el-table :data="form.args" height="300" stripe border style="width: 100%;">
-                    <el-table-column label="名称">
-                        <template slot-scope="scope">
-                            <span v-if="!scope.row.edit" slot="reference" class="name-wrapper">{{ scope.row.name }}</span>
-                            <span v-if="scope.row.edit" class="cell">
-                                <el-input v-model="scope.row.name" placeholder="名称"></el-input>
-                            </span>
-                        </template>
-                    </el-table-column>
+                <el-table :data="form.args" height="400" stripe border style="width: 100%;">
                     <el-table-column label="变量名">
                         <template slot-scope="scope">
                             <span v-if="!scope.row.edit" slot="reference" class="name-wrapper">{{ scope.row.variable }}</span>
@@ -69,13 +54,11 @@
                             </span>
                         </template>
                     </el-table-column>
-                    <el-table-column label="变量类型">
+                    <el-table-column label="描述">
                         <template slot-scope="scope">
-                            <span v-if="!scope.row.edit" slot="reference" class="name-wrapper">{{ scope.row.type }}</span>
+                            <span v-if="!scope.row.edit" slot="reference" class="name-wrapper">{{ scope.row.desc }}</span>
                             <span v-if="scope.row.edit" class="cell">
-                                <el-select v-model="scope.row.type" placeholder="变量类型" clearable filterable>
-                                    <el-option v-for="item in variableTypes" :key="item" :label="item" :value="item"/>
-                                </el-select>
+                                <el-input v-model="scope.row.desc" placeholder="名称"></el-input>
                             </span>
                         </template>
                     </el-table-column>
@@ -158,12 +141,8 @@ export default {
             return row.environ
         },
         savePlugArg(row) {
-            if(row.name == "" || row.type == "" || row.value == "" || row.variable == "") {
+            if(row.value == "" || row.variable == "") {
                 utils.showFailed(this, "参数不能为空")
-                return
-            }
-            if (utils.array_count(this.form.args, row.name, "name") > 1) {
-                utils.showFailed(this, "已经存在同名参数")
                 return
             }
             if (utils.array_count(this.form.args, row.variable, "variable") > 1) {
@@ -175,7 +154,7 @@ export default {
         },
         addPlugArg() {
             this.argEdit = true
-            this.form.args.push({ edit: true, variable: "", name: "", type: "", value : "" })
+            this.form.args.push({ edit: true, variable: "", desc: "", value : "" })
         },
         editPlugArg(row) {
             if (!this.argEdit) {
@@ -184,7 +163,7 @@ export default {
             }
         },
         delPlugArg(row) {
-            utils.array_remove(this.form.args, row.name, "name")
+            utils.array_remove(this.form.args, row.variable, "variable")
         },
         resetForm() {
             this.argEdit = false
@@ -207,8 +186,8 @@ export default {
             var fargs = []
             var form = Object.assign({}, this.form) // copy obj
             for (var arg of form.args) {
-                if (arg.type != "" && arg.name != "" && arg.value != "" && arg.variable != "") {
-                    fargs.push({type: arg.type, value: arg.value, variable: arg.variable, name: arg.name })
+                if (arg.value != "" && arg.variable != "") {
+                    fargs.push({desc: arg.desc, value: arg.value, variable: arg.variable })
                 }
             }
             form.args = fargs
@@ -232,7 +211,7 @@ export default {
             this.argEdit = false
             this.form.id = null
             for (var arg of row.args) {
-                this.form.args.push({edit: false, type: arg.type, value: arg.value, variable: arg.variable, name: arg.name })
+                this.form.args.push({edit: false, desc: arg.desc, value: arg.value, variable: arg.variable })
             }
             this.dialogStatus = 'create'
             this.dialogFormVisible = true
@@ -245,7 +224,7 @@ export default {
             this.form.args = []
             this.argEdit = false
             for (var arg of row.args) {
-                this.form.args.push({edit: false, type: arg.type, value: arg.value, variable: arg.variable, name: arg.name })
+                this.form.args.push({edit: false, desc: arg.desc, value: arg.value, variable: arg.variable })
             }
             this.dialogStatus = 'update'
             this.dialogFormVisible = true
